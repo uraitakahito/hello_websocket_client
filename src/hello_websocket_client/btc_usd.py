3dbeff444e830319882ccac7e08a42758b22f53a
Takahito Urai <uraitakahito@gmail.com>
2025-04-23 13:29:11 +0900

    Refactor websocket callback functions to include type hints for better clarity and maintainability

--- src/hello_websocket_client/btc_usd.py
+++ src/hello_websocket_client/btc_usd.py
@@ -1,26 +1,33 @@
-import websocket
 import rel
+import websocket
 
-def on_message(ws, message):
+
+def on_message(ws: websocket.WebSocket, message: str):
     print(message)
 
-def on_error(ws, error):
+
+def on_error(ws: websocket.WebSocket, error: Exception):
     print(error)
 
-def on_close(ws, close_status_code, close_msg):
+
+def on_close(ws: websocket.WebSocket, close_status_code: str, close_msg: str):
     print("### closed ###")
 
-def on_open(ws):
+
+def on_open(ws: websocket.WebSocket):
     print("Opened connection")
+
 
 if __name__ == "__main__":
     # websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/BTCUSD",
-                              on_open=on_open,
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
+    ws = websocket.WebSocketApp(
+        "wss://api.gemini.com/v1/marketdata/BTCUSD",
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close,
+    )
 
-    ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
-    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    ws.run_forever(dispatcher=rel, reconnect=5)  # type: ignore
+    rel.signal(2, rel.abort)  # type: ignore
     rel.dispatch()
